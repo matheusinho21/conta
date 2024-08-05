a4edebce81284082f43aaa6a06809fe658c62be7
projeto 1 <129951535+matheusinho21@users.noreply.github.com>
2024-08-05 08:58:36 -0300

    Create conta.py

---
+++ conta.py
@@ -0,0 +1,14 @@
+class conta:
+    def __ini__(Self, numero, titular, saldoinicial, limite):
+        Self.numero = numero
+        Self.titular = titular
+        Self.saldo = saldoinicial
+        Self.limite = limite
+
+
+
+def extrato(self):
+    print("saldo atual: {}".format(self._saldo))
+
+def __podeSacar(self, valorSacar):
+    valorDisponivel = self.__saldo + self.__limite    
